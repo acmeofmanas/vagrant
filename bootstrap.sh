faf1ff8a6c5d32b7e04a6da0568076e71bfe4b64
Manas Pradhan <manaspradhan@Manass-MacBook-Pro.local>
2022-01-21 11:26:53 +0800

    bootstrap point k8 repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,15 +44,14 @@ swapoff -a
 
 # Add yum repo file for Kubernetes
 echo "[TASK 8] Add yum repo file for kubernetes"
-cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
+cat >/etc/yum.repos.d/kubernetes.repo<<EOF
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-$basearch
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kubelet kubeadm kubectl
 EOF
 
 # Install Kubernetes
